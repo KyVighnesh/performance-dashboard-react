93b46e65b96a7b883ebcab840aae8374382872f1
KyVighnesh <110763997+KyVighnesh@users.noreply.github.com>
2023-07-17 11:36:07 +0530

    Update Dashboard.jsx

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -9,6 +9,7 @@ import {
   Legend,
   CartesianGrid,
   Bar,
+  ResponsiveContainer
 } from "recharts";
 
 import Anim from "../Anim.json"
@@ -16,6 +17,8 @@ import Lottie from "lottie-react"
 
 import axios from "axios";
 import { useState,useEffect } from "react";
+import { useNavigate } from 'react-router-dom'
+
 
 
 const Dashboard = () => {
@@ -27,6 +30,15 @@ const Dashboard = () => {
     const[bgColor,setbgColor] = useState()
 
     const [move,setFloat] = useState ()
+
+
+    const navigate = useNavigate()
+
+
+
+    const logOut = () => {
+        navigate("/")
+    }
 
 
     const onClickToggle = () => {
@@ -53,20 +65,23 @@ const Dashboard = () => {
     },[])
     
       return (
-        <div style={{backgroundColor:bgColor}}>
+        <div style={{backgroundColor:bgColor,height:"94vh"}}>
           
 <nav class="navbar navbar-light bg-light">
   <div class="container-fluid">
     <span class="navbar-brand mb-0 h1">Dashboard</span>
+    <button type="button" class="btn btn-primary" onClick={logOut}>Log out</button>
+
   </div>
 </nav>
 
-<div style={{display:'flex'}}>
-          <div className="App" style={{marginTop:"30px"}}>
+<div style={{display:'flex',width:"100%",height:"100%",boxSizing:"border-box",marginTop:"20px"}}>
+          <div className="App" style={{marginTop:"30px",width:"90%",height:"60vh"}}>
+
+          <ResponsiveContainer>
 
             <BarChart
-              width={1200}
-              height={600}
+              
               data={data}
               margin={{
                 top: 5,
@@ -87,18 +102,22 @@ const Dashboard = () => {
               <CartesianGrid strokeDasharray="3 3" />
               <Bar dataKey="performance" fill="#8884d8" background={{ fill: "#eee" }} />
             </BarChart>
+            </ResponsiveContainer>
           </div>
 
-          <div style={{height:"38px", width:"120px", borderRadius:"20px",backgroundColor:"silver",marginLeft:"100px",cursor:"pointer"}} onClick = {onClickToggle}>
-            <button style={{height:"35px",width:"35px",borderRadius:"20px",marginLeft:move,transition:"linear 0.5s"}}>
+          <div style={{marginLeft:"20px",width:"20%",display:"flex",flexDirection:"column",alignItems:"center"}}>
+
+          <div style={{height:"38px", width:"8vw", borderRadius:"20px",backgroundColor:"silver",cursor:"pointer"}} onClick = {onClickToggle}>
+            <button style={{height:"100%",width:"40%",borderRadius:"20px",marginLeft:move,transition:"linear 0.5s"}}>
 
             </button>
           </div>
-          <div style={{height:"40px",marginTop:"30px",width:"120px"}}>
-          <Lottie style = {{height:"80px",position:"relative",right:80}}animationData={Anim} />
-          <span style={{position:"relative",right:80,fontWeight:10}}>Dark Mode</span>
+          <div style={{height:"40px",width:"20%"}}>
+          <Lottie style = {{height:"80px"}}animationData={Anim} />
+          <span style={{position:"relative",fontWeight:10}}>Dark Mode</span>
           </div>
           </div>    
+          </div>
         </div>
       );
 }
